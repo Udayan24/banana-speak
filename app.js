792370c1660df09d283c840f4356c5d35e4fc2aa
Udayan Chavan <udayanchavan24@gmail.com>
2023-01-19 23:14:22 +0530

    Added translation through api

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var btnTranslate = document.querySelector("#btn-translate")
+btnTranslate.addEventListener("click", clickHandler)
+
 var textareaInput = document.querySelector("#textarea-input")
 var textareaOutput = document.querySelector("#textarea-output")
 
+var url = "https://api.funtranslations.com/translate/minion.json"
+
 function clickHandler(){
-    console.log(textareaInput.value)
-    textareaOutput.textContent = textareaInput.value
+    var input = textareaInput.value;
+    var finalIP = constructURL(input)
+
+    fetch(finalIP)
+        .then(response => response.json())
+        .then(json => {
+            textareaOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("Error")) 
 }
 
-btnTranslate.addEventListener("click", clickHandler)
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText)
+    return `${url}?text=${encodedURI}`
+}
